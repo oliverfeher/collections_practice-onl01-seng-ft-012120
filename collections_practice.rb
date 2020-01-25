033ba53bc9e6eb040009d1ac129351f538cf010d
Oliver Feher <olcsee@gmail.com>
2020-01-25 06:36:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,3 +15,29 @@ def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.map { |word| word.sub(word[2], "$")}
+end 
+
+def find_a(array)
+  array.find_all { |word| word[0] == "a"}
+end
+
+def sum_array(array)
+  array.inject(0) { |accumulator, number| accumulator + number}
+end
+
+def add_s(array)
+  array.collect { |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  }
+end
